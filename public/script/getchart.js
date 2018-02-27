9e56ee3cdd27349b90ff7c4053ae520e72dda4c6
Karthik Ram <karthik@Karthiks-MacBook-Pro.local>
2018-02-27 06:49:46 +0530

    graph update

--- public/script/getchart.js
+++ public/script/getchart.js
@@ -1,20 +1,22 @@
-//var ChartData;
+var ChartData;
 
-$(() => {
+function chartDemo(){
+
     $.ajax({
 
         url: 'http://localhost:3000/home',
         type: 'GET',
         success : (data) => {
             //ChartData = data;
+            console.log(data);
             var template = Handlebars.compile($('#tabular-template').html());
             $("#table-location").html(template(data));
             
-            var Chart1 = document.getElementById('chart-location').getContext('2d');
+            var Chart1 = document.getElementById('chart-location');
             const firstChart = new Chart(Chart1, {
                 type: 'line',
-                labels: data['categories'],
-                data: data['dataset'],
+                labels: data.categories,
+                dataset: data.dataset,
                 options: {
                     title: {
                         display: true,
@@ -22,6 +24,56 @@ $(() => {
                     }
                 }
             });
+        //Chart1.render();    
         }
     });
-});
+
+}
+
+
+
+
+// function chartDemo() {
+
+//     var ctx = document.getElementById("chart-location").getContext('2d');
+//             var myChart = new Chart(ctx, {
+//                 type: 'bar',
+//                 data: {
+//                     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//                     datasets: [{
+//                         label: '# of Votes',
+//                         data: [12, 19, 3, 5, 2, 3],
+//                         backgroundColor: [
+//                             'rgba(255, 99, 132, 0.2)',
+//                             'rgba(54, 162, 235, 0.2)',
+//                             'rgba(255, 206, 86, 0.2)',
+//                             'rgba(75, 192, 192, 0.2)',
+//                             'rgba(153, 102, 255, 0.2)',
+//                             'rgba(255, 159, 64, 0.2)'
+//                         ],
+//                         borderColor: [
+//                             'rgba(255,99,132,1)',
+//                             'rgba(54, 162, 235, 1)',
+//                             'rgba(255, 206, 86, 1)',
+//                             'rgba(75, 192, 192, 1)',
+//                             'rgba(153, 102, 255, 1)',
+//                             'rgba(255, 159, 64, 1)'
+//                         ],
+//                         borderWidth: 1
+//                     }]
+//                 },
+//                 options: {
+//                     scales: {
+//                         yAxes: [{
+//                             ticks: {
+//                                 beginAtZero:true
+//                             }
+//                         }]
+//                     }
+//                 }
+//             });
+            
+// }
+            
+  
+
